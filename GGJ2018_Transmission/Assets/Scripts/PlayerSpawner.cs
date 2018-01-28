7fd5be5ee852a76d37ae58e7116ad44ac72b479f
Luce <lucy_liu@live.ca>
2018-01-28 14:09:58 -0800

    add now

--- GGJ2018_Transmission/Assets/Scripts/PlayerSpawner.cs
+++ GGJ2018_Transmission/Assets/Scripts/PlayerSpawner.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
 public class PlayerSpawner : MonoBehaviour
 {
-  public Player spawnA;
-  public Player spawnB;
-  public Player spawnX;
-  public Player spawnY;
-
+  //public Player spawnMyPlayer;
+  public int SECONDS_TIL_SPAWN = 10;
   void Awake() {
-    InvokeRepeating ("Spawn", 0f, spawnDelay);
+    //InvokeRepeating ("Spawn", 0f, spawnDelay);
   }
 
-  void SpawnDead() {
+  void Spawn(Player spawnMe) {
+    Invoke("ExecuteSpawn", SECONDS_TIL_SPAWN);
+  }
 
+  void ExecuteSpawn(Player spawnMe) {
+    //if (playerID =)
+    spawnMe.SetActive(true);
   }
 }
