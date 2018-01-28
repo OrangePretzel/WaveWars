8276b28e9369aab07b9d7e89d7fff0dcd4ee1f83
SevenSidedVoxel <sevensidedvoxel@gmail.com>
2018-01-27 17:13:46 -0800

    Added Keyboard/Mouse support

--- GGJ2018_Transmission/Assets/Scripts/Player.cs
+++ GGJ2018_Transmission/Assets/Scripts/Player.cs
@@ -1,28 +1,27 @@
 using UnityEngine;
-using System.Collections;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
-public Rigidbody2D rb;
-public float speed;
-public int playerID = 0;
+	public Rigidbody2D rb;
+	public float speed;
+	public int playerID = 0;
 
-void Awake()
- {
-     rb = GetComponent<Rigidbody2D>();
-     GameManager.Instance.InitGameManager(this);
- }
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
 
-void Update()
-{
-  var playerInput = InputManager.GetPlayerInput(playerID);
-  rb.velocity = new Vector2(playerInput.HorizontalMovement*speed, playerInput.VerticalMovement*speed);
-  rb.MovePosition(rb.position + rb.velocity * Time.deltaTime);
-  var playerPosition = this.transform.position;
-  var ray = new Vector3(playerInput.HorizontalAim, playerInput.VerticalAim, 0);
-  Debug.Log(ray);
-  Debug.DrawRay(playerPosition, ray, Color.yellow, 5.0f, true);
-}
+	void Update()
+	{
+		var playerInput = InputManager.GetPlayerInput(playerID);
+		var aimVec = playerInput.GetNormalizedAim(transform.position, Camera.main);
 
+		rb.velocity = new Vector2(playerInput.HorizontalMovement * speed, playerInput.VerticalMovement * speed);
+		rb.MovePosition(rb.position + rb.velocity * Time.deltaTime);
+		var playerPosition = this.transform.position;
+		var ray = new Vector3(aimVec.x, aimVec.y, 0);
+		Debug.Log(ray);
+		Debug.DrawRay(playerPosition, ray, Color.yellow, 5.0f, true);
+	}
 }
