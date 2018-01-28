f8435fa26c8d57696b5bddadc34acfcf3b69bdcd
Luce <lucy_liu@live.ca>
2018-01-27 18:17:11 -0800

    starting work on charge

--- GGJ2018_Transmission/Assets/Scripts/Player.cs
+++ GGJ2018_Transmission/Assets/Scripts/Player.cs
@@ -1,60 +1,67 @@
 using UnityEngine;
+using System.Collections;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
-	public Rigidbody2D rb;
-	public float speed;
-	public int playerID = 0;
+public Rigidbody2D rb;
+public float speed;
+public int playerID = 0;
+private int transmission = 0;
+private PlayerInput playerInput;
+private int charge;
+void Awake()
+ {
+     rb = GetComponent<Rigidbody2D>();
+     GameManager.Instance.InitGameManager(this);
+     charge=100;
+ }
 
-	private int transmission;
-	private PlayerInput playerInput;
+void Update()
+{
+  playerInput = InputManager.GetPlayerInput(playerID);
+  rb.velocity = new Vector2(playerInput.HorizontalMovement*speed, playerInput.VerticalMovement*speed);
+  rb.MovePosition(rb.position + rb.velocity * Time.deltaTime);
+  var playerPosition = this.transform.position;
+  var ray = new Vector3(playerInput.HorizontalAim, playerInput.VerticalAim, 0);
+  //Debug.Log(ray);
 
-	void Awake()
-	{
-		rb = GetComponent<Rigidbody2D>();
-	}
+  if (transmission==1){
+    Debug.DrawRay(playerPosition, ray, Color.yellow, 5.0f, true);
+    charge--;
+  }
+  else if (transmission==2){
+    Debug.DrawRay(playerPosition, ray, Color.blue, 5.0f, true);
+    charge--;
+  }
+  else if (transmission==0 && charge<100){
+    charge++;
+  }
 
-	void Update()
-	{
-		playerInput = InputManager.GetPlayerInput(playerID);
-		var aimVec = playerInput.GetNormalizedAim(transform.position, Camera.main);
+  Debug.Log("Charge: "+charge);
+  //Debug.Log("Pull: "+playerInput.PullTransmission+", Push: "+playerInput.PushTransmission);
+  HandleTransmission();
+  Debug.Log("Transmission set to: "+transmission);
+}
 
-		rb.velocity = new Vector2(playerInput.HorizontalMovement * speed, playerInput.VerticalMovement * speed);
-		rb.MovePosition(rb.position + rb.velocity * Time.deltaTime);
-		var playerPosition = this.transform.position;
-		var ray = new Vector3(aimVec.x, aimVec.y, 0);
-		Debug.Log(ray);
+void HandleTransmission(){
+  // currently nothing, switch to push
+  if (transmission==0 && playerInput.PushTransmission && charge>0){
+    transmission=1;
+  }
+  // currently nothing, switch to pull
+  else if (transmission==0 && playerInput.PullTransmission && charge>0){
+    transmission=2;
+  }
+  else if (!playerInput.PushTransmission && !playerInput.PullTransmission){
+    transmission=0;
+  }
+  else if (transmission==1 && charge<0){
+    transmission=0;
+  }
+  else if (transmission==2 && charge<0){
+    transmission=0;
+  }
+}
 
-		if (transmission == 1)
-		{
-			Debug.DrawRay(playerPosition, ray, Color.yellow, 5.0f, true);
-		}
-		else if (transmission == 2)
-		{
-			Debug.DrawRay(playerPosition, ray, Color.blue, 5.0f, true);
-		}
-
-		//Debug.Log("Pull: "+playerInput.PullTransmission+", Push: "+playerInput.PushTransmission);
-		HandleTransmission();
-		Debug.Log("Transmission set to: " + transmission);
-	}
-
-	void HandleTransmission()
-	{
-		// currently nothing, switch to push
-		if (transmission == 0 && playerInput.PushTransmission)
-		{
-			transmission = 1;
-		}
-		// currently nothing, switch to pull
-		else if (transmission == 0 && playerInput.PullTransmission)
-		{
-			transmission = 2;
-		}
-		else if (!playerInput.PushTransmission && !playerInput.PullTransmission)
-		{
-			transmission = 0;
-		}
-	}
 }
