8be72d8eb0ff103a38963b54d5e2b0657d6643fb
SevenSidedVoxel <sevensidedvoxel@gmail.com>
2018-01-28 14:47:59 -0800

    Player faces dir

--- GGJ2018_Transmission/Assets/Scripts/Player.cs
+++ GGJ2018_Transmission/Assets/Scripts/Player.cs
@@ -5,101 +5,107 @@ using System.Collections;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Player : Entity
 {
-    public float TransmittingVelocityModifier = 0.75f;
+	public float TransmittingVelocityModifier = 0.75f;
 
-    public new Rigidbody2D rigidbody;
-    public float speed;
-    private PlayerInput playerInput;
-    private WaveEmitter waveEmitter;
+	public new Rigidbody2D rigidbody;
+	public float speed;
+	private PlayerInput playerInput;
+	private WaveEmitter waveEmitter;
 
-    public float MaxCharge = 10;
+	public float MaxCharge = 10;
 
-    [SerializeField]
-    private float charge;
+	[SerializeField]
+	private float charge;
 
-    public Canvas healthbarCanvas;
-    public Image healthbar;
-    public ParticleSystem deathParticles;
+	public Canvas healthbarCanvas;
+	public Image healthbar;
+	public ParticleSystem deathParticles;
 
-    void Awake()
-    {
-        hp = maxHP;
-        healthbarCanvas.enabled = false;
-        rigidbody = GetComponent<Rigidbody2D>();
-        charge = MaxCharge;
-        playerInput = InputManager.GetPlayerInput(PlayerID);
-        waveEmitter = GetComponentInChildren<WaveEmitter>();
-        waveEmitter?.SetEntityProperties(teamID, playerID, reflective);
-    }
+	void Awake()
+	{
+		hp = maxHP;
+		healthbarCanvas.enabled = false;
+		rigidbody = GetComponent<Rigidbody2D>();
+		charge = MaxCharge;
+		playerInput = InputManager.GetPlayerInput(PlayerID);
+		waveEmitter = GetComponentInChildren<WaveEmitter>();
+		waveEmitter?.SetEntityProperties(teamID, playerID, reflective);
+	}
 
-    void Update()
-    {
-        if (GameManager.IsGamePaused)
-            return;
+	void Update()
+	{
+		if (GameManager.IsGamePaused)
+			return;
 
-        // Check if the player should be dead.
-        if (hp <= 0)
-        {
-            Instantiate(deathParticles, transform.position, new Quaternion());
-            gameObject.SetActive(false);
-            // Do stuff for respawn.
-        }
+		// Check if the player should be dead.
+		if (hp <= 0)
+		{
+			Instantiate(deathParticles, transform.position, new Quaternion());
+			gameObject.SetActive(false);
+			// Do stuff for respawn.
+		}
 
-        playerInput = InputManager.GetPlayerInput(playerID);
+		playerInput = InputManager.GetPlayerInput(playerID);
 
-        var aimVec = playerInput.GetNormalizedAim(transform.position, Camera.main);
-        bool isTransmitting = aimVec.sqrMagnitude != 0
-            && (playerInput.PlayerDevice != InputManager.KEYBOARD_AND_MOUSE || Input.GetKey(KeyCode.Mouse0));
+		var aimVec = playerInput.GetNormalizedAim(transform.position, Camera.main);
+		bool isTransmitting = aimVec.sqrMagnitude != 0
+			&& (playerInput.PlayerDevice != InputManager.KEYBOARD_AND_MOUSE || Input.GetKey(KeyCode.Mouse0));
 
-        rigidbody.velocity = new Vector2(playerInput.HorizontalMovement, playerInput.VerticalMovement)
-            * speed
-            * (isTransmitting ? TransmittingVelocityModifier : 1);
-        rigidbody.MovePosition(rigidbody.position + rigidbody.velocity * Time.deltaTime);
+		rigidbody.velocity = new Vector2(playerInput.HorizontalMovement, playerInput.VerticalMovement)
+			* speed
+			* (isTransmitting ? TransmittingVelocityModifier : 1);
+		rigidbody.MovePosition(rigidbody.position + rigidbody.velocity * Time.deltaTime);
 
-        if (isTransmitting && charge > 0)
-        {
-            if (!GameManager.GameSettings.UnlimitedTransmission)
-                charge -= Time.deltaTime;
+		if (rigidbody.velocity.x != 0)
+		{
+			var dir = Mathf.Sign(rigidbody.velocity.x);
+			transform.localScale = new Vector3(dir, 1, 1);
+		}
 
-            ShootTransmission(aimVec);
-            //Debug.DrawRay(playerPosition, aimVec, Color.yellow, 5.0f, true);
-        }
+		if (isTransmitting && charge > 0)
+		{
+			if (!GameManager.GameSettings.UnlimitedTransmission)
+				charge -= Time.deltaTime;
 
-        if (!isTransmitting || charge <= 0)
-        {
-            waveEmitter?.StopTransmit();
-        }
+			ShootTransmission(aimVec);
+			//Debug.DrawRay(playerPosition, aimVec, Color.yellow, 5.0f, true);
+		}
 
-        if (!isTransmitting && charge < MaxCharge)
-        {
-            charge += Time.deltaTime;
-            if (charge >= MaxCharge)
-                charge = MaxCharge;
-        }
-    }
+		if (!isTransmitting || charge <= 0)
+		{
+			waveEmitter?.StopTransmit();
+		}
 
-    private void ShootTransmission(Vector3 direction)
-    {
-        if (waveEmitter)
-        {
-            var q = new Quaternion();
-            q.SetLookRotation(Vector3.forward, direction);
-            waveEmitter.transform.localRotation = q;
-            waveEmitter.Transmit();
-        }
-    }
+		if (!isTransmitting && charge < MaxCharge)
+		{
+			charge += Time.deltaTime;
+			if (charge >= MaxCharge)
+				charge = MaxCharge;
+		}
+	}
 
-    public override void UpdateHealthBar()
-    {
-        if (maxHP > hp)
-        {
-            healthbarCanvas.enabled = true;
-            healthbar.fillAmount = hp / maxHP;
-        }
-        else
-        {
-            healthbarCanvas.enabled = false;
-            healthbar.fillAmount = 1;
-        }
-    }
+	private void ShootTransmission(Vector3 direction)
+	{
+		if (waveEmitter)
+		{
+			var q = new Quaternion();
+			q.SetLookRotation(Vector3.forward, direction);
+			waveEmitter.transform.localRotation = q;
+			waveEmitter.Transmit();
+		}
+	}
+
+	public override void UpdateHealthBar()
+	{
+		if (maxHP > hp)
+		{
+			healthbarCanvas.enabled = true;
+			healthbar.fillAmount = hp / maxHP;
+		}
+		else
+		{
+			healthbarCanvas.enabled = false;
+			healthbar.fillAmount = 1;
+		}
+	}
 }
