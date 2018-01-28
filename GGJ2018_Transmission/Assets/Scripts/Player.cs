f922217a5109e54f531433d884e12b02422c9fa4
Luce <lucy_liu@live.ca>
2018-01-27 19:23:00 -0800

    commit b4 pulling from origin

--- GGJ2018_Transmission/Assets/Scripts/Player.cs
+++ GGJ2018_Transmission/Assets/Scripts/Player.cs
@@ -10,12 +10,16 @@ public int playerID = 0;
 private bool transmission;
 private PlayerInput playerInput;
 private int charge;
+private Vector3 ray;
+private Vector3 playerPosition;
 void Awake()
  {
      rb = GetComponent<Rigidbody2D>();
      GameManager.Instance.InitGameManager(this);
      charge=100;
      transmission=false;
+     playerInput = InputManager.GetPlayerInput(playerID);
+     ray = new Vector3(playerInput.HorizontalAim, playerInput.VerticalAim, 0);
  }
 
 void Update()
@@ -23,38 +27,34 @@ void Update()
   playerInput = InputManager.GetPlayerInput(playerID);
   rb.velocity = new Vector2(playerInput.HorizontalMovement*speed, playerInput.VerticalMovement*speed);
   rb.MovePosition(rb.position + rb.velocity * Time.deltaTime);
-  var playerPosition = this.transform.position;
-  var ray = new Vector3(playerInput.HorizontalAim, playerInput.VerticalAim, 0);
-  //Debug.Log(ray);
+  playerPosition = this.transform.position;
+  ray = new Vector3(playerInput.HorizontalAim, playerInput.VerticalAim, 0);
 
-  if (transmission){
-    Debug.DrawRay(playerPosition, ray, Color.yellow, 5.0f, true);
-    charge--;
-  }
-  else if (!transmission && charge<100){
-    charge++;
-  }
-
-  Debug.Log("Charge: "+charge);
-  //Debug.Log("Pull: "+playerInput.PullTransmission+", Push: "+playerInput.PushTransmission);
   HandleTransmission();
   Debug.Log("Transmission: "+transmission);
+  ChargeWhileIdle();
+  Debug.Log("Charge: "+charge);
 }
 
 void HandleTransmission(){
   // currently nothing, switch to push
-  if (!transmission && playerInput.PushTransmission && charge>0 && Aiming()){
+  if ((playerInput.PushTransmission || playerInput.PullTransmission) && charge>0 && Aiming()){
     transmission=true;
-  }
-  // currently nothing, switch to pull
-  else if (!transmission && playerInput.PullTransmission && charge>0 && Aiming()){
-    transmission=true;
+    charge--;
+    Debug.DrawRay(playerPosition, ray, Color.yellow, 5.0f, true);
   }
   else if (!playerInput.PushTransmission && !playerInput.PullTransmission && Aiming()){
     transmission=false;
   }
   else if (transmission && charge<=0){
     transmission=false;
+    Debug.Log("CHARGE LESS THAN OR EQUAL TO ZERO");
+  }
+}
+
+void ChargeWhileIdle(){
+  if (!transmission && charge<100){
+    charge++;
   }
 }
 
