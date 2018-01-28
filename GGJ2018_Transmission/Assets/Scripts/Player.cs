0c9ba03402c4f27ebc65e2f8578888f8cfb4959a
Luce <lucy_liu@live.ca>
2018-01-27 16:22:28 -0800

    can now draw debug rays with right joystick

--- GGJ2018_Transmission/Assets/Scripts/Player.cs
+++ GGJ2018_Transmission/Assets/Scripts/Player.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
+using System.Collections;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
 public Rigidbody2D rb;
 public float speed;
-//public playerID id;
+public int playerID = 0;
 
 void Awake()
  {
@@ -15,9 +16,13 @@ void Awake()
 
 void Update()
 {
-  var playerInput = InputManager.GetPlayerInput(0);
+  var playerInput = InputManager.GetPlayerInput(playerID);
   rb.velocity = new Vector2(playerInput.HorizontalMovement*speed, playerInput.VerticalMovement*speed);
-  rb.MovePosition(rb.position + rb.velocity * Time.fixedDeltaTime);
+  rb.MovePosition(rb.position + rb.velocity * Time.deltaTime);
+  var playerPosition = this.transform.position;
+  var ray = new Vector3(playerInput.HorizontalAim, playerInput.VerticalAim, 0);
+  Debug.Log(ray);
+  Debug.DrawRay(playerPosition, ray, Color.yellow, 5.0f, true);
 }
 
 }
