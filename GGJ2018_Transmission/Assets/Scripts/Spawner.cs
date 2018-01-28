c2e6aad5657e0eece7de6e75ad2989c509753fce
SevenSidedVoxel <sevensidedvoxel@gmail.com>
2018-01-28 14:44:47 -0800

    thungs

--- GGJ2018_Transmission/Assets/Scripts/Spawner.cs
+++ GGJ2018_Transmission/Assets/Scripts/Spawner.cs
@@ -2,36 +2,48 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-  public int TEAM_ID;
-  public float spawnDelay = 5;
-  public GameObject thingToSpawn;
-  public GameObject minions;
+	public int TEAM_ID;
+	public float spawnDelay = 5;
+	public GameObject thingToSpawn;
+	public GameObject minions;
 
-  void Awake() {
-    InvokeRepeating ("Spawn", 0f, spawnDelay);
-  }
+	public float lastSpawn;
 
-  void Update() {
+	void FixedUpdate()
+	{
+		if (GameManager.IsGamePaused)
+			return;
 
-  }
+		var time = Time.time;
+		var diff = time - lastSpawn;
+		if (diff > spawnDelay)
+		{
+			Spawn();
+			lastSpawn = time;
+		}
+	}
 
-  void Spawn() {
-    // spawn anything
-    GameObject newMin = Instantiate(thingToSpawn, this.transform);
-    newMin.transform.SetParent(minions.transform);
-    var MinionEntity = newMin.GetComponent<Entity>();
-    var MinionType = newMin.GetComponent<Minion>();
-    GameManager.AddEntity(MinionEntity); // make sure GameManager is aware of new minion
-    PushNewlySpawned(MinionEntity, MinionType); // push it a lil so they don't all overlap
-  }
+	void Spawn()
+	{
+		// spawn anything
+		GameObject newMin = Instantiate(thingToSpawn, this.transform.position, Quaternion.identity, minions.transform);
+		var MinionEntity = newMin.GetComponent<Entity>();
+		var MinionType = newMin.GetComponent<Minion>();
+		GameManager.AddEntity(MinionEntity); // make sure GameManager is aware of new minion
+		PushNewlySpawned(MinionEntity, MinionType); // push it a lil so they don't all overlap
+	}
 
-  void PushNewlySpawned(Entity me, Minion mt) {
-    Vector2 forceA = new Vector2(1,0);
-    Vector2 forceB = new Vector2(-1,0);
-    if (TEAM_ID==0) {
-      mt.GetComponent<Rigidbody2D>().AddForce(forceA);
-    } else {
-      mt.GetComponent<Rigidbody2D>().AddForce(forceB);
-    }
-  }
+	void PushNewlySpawned(Entity me, Minion mt)
+	{
+		Vector2 forceA = new Vector2(1, 0);
+		Vector2 forceB = new Vector2(-1, 0);
+		if (TEAM_ID == 0)
+		{
+			mt.GetComponent<Rigidbody2D>().AddForce(forceA);
+		}
+		else
+		{
+			mt.GetComponent<Rigidbody2D>().AddForce(forceB);
+		}
+	}
 }
