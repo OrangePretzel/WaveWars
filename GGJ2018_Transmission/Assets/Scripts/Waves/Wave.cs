cc0dfbe554e3af26dacac324a0c9235a7fa1c434
NICooper <nicholas.i.cooper@gmail.com>
2018-01-28 02:06:37 -0800

    Wave Visual

--- GGJ2018_Transmission/Assets/Scripts/Waves/Wave.cs
+++ GGJ2018_Transmission/Assets/Scripts/Waves/Wave.cs
@@ -7,10 +7,12 @@ public class Wave : Entity
     public Wavelet waveletPrefab;
 
     public float wavePower = 10.0f;
-    public List<Wavelet> wavelets;
-    public int angle = 30;
+    public float speed = 2.0f;
+    public float density = 2.0f;
+    public float angle = 20;
     public int numWavelets = 5;
-    public float distToWaveletSpawn = 0.5f;
+    public float distToWaveletSpawn = 0.2f;
+    public List<Wavelet> wavelets;
 
     void Start()
     {
@@ -19,17 +21,38 @@ public class Wave : Entity
 
     private void CreateWavelets()
     {
+        wavelets.Clear();
+        float waveletAngle = angle / numWavelets;
+        float spawnAngle = (-angle / 2) + (waveletAngle / 2);
+        float arcLength = distToWaveletSpawn * Mathf.Deg2Rad * angle;
+        float waveletWidth = arcLength / numWavelets;
+
         for (int i = 0; i < numWavelets; i++)
         {
             var w = Instantiate(waveletPrefab, transform);
-            w.transform.parent = transform;
+            w.transform.Rotate(Vector3.forward, -spawnAngle);
+            w.transform.Translate(distToWaveletSpawn * Mathf.Sin(Mathf.Deg2Rad * spawnAngle),
+                                  distToWaveletSpawn * Mathf.Cos(Mathf.Deg2Rad * spawnAngle),
+                                  0.0f, transform);
+            w.Initialize(wavePower, waveletWidth, waveletAngle, speed, density);
             wavelets.Add(w);
+
+            spawnAngle += waveletAngle;
         }
     }
 
     void Scan()
     {
 
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Disengage();
+            CreateWavelets();
+        }
     }
 
     public void UpdateWaveAngle(int newAngle)
@@ -45,10 +68,10 @@ public class Wave : Entity
         // For testing. Later this should just prepare the wave to collapse.
         for (int i = 0; i < wavelets.Count; i++)
         {
-            var w = wavelets[i];
-            wavelets.Remove(w);
-            Destroy(w);
+            Destroy(wavelets[i].gameObject);
         }
-        Destroy(this);
+        wavelets.Clear();
+        //Destroy(this);
     }
+
 }
--- GGJ2018_Transmission/Assets/Scripts/Waves/Wavelet.cs
+++ GGJ2018_Transmission/Assets/Scripts/Waves/Wavelet.cs
@@ -3,30 +3,46 @@ using System.Collections;
 
 public class Wavelet: MonoBehaviour
 {
-    public float wavePower = 10.0f;
-    public float maxLength = 0.0f;
-    public float length = 0.0f;
-    public float speed = 0.5f;
-    public float scrollSpeed = -0.1f;
-    public float waveDensity = 2.0f;
+    private float wavePower = 10.0f;
+    private float maxLength = 0.0f;
+    private float length = 0.0f;
+    private float speed = 0.5f;
+    private float waveDensity = 2.0f;
 
-    public float angle = 10.0f;
-    public float baseWidth = 0.3f;
+    private float angle = 4.0f;
+    private float baseWidth = 0.3f;
 
+    private float phase = 0.0f;
+    private float tiling = 0.0f;
     private MeshFilter mf;
     private Mesh mesh;
     public Vector3[] vertices;
     public Vector2[] uvs;
     private float spreadRatio;
     private float halfWidth;
+    public float wavefrontOffset = 0.0f;
+    public float leftover;
 
     void Start()
     {
+        
+        
+    }
+
+    public void Initialize(float power, float width, float angle, float speed, float density)
+    {
+
+        baseWidth = width;
+        halfWidth = baseWidth / 2.0f;
+        this.angle = angle;
+        wavePower = power;
+        maxLength = wavePower;
+        this.speed = speed;
+        waveDensity = density;
+
         // Find the half angle, convert to radians, and use tan() to get the ratio of
         // of length to width.
-        spreadRatio = Mathf.Tan(Mathf.Deg2Rad * (angle / 2.0f));
-        halfWidth = baseWidth / 2.0f;
-        maxLength = wavePower;
+        spreadRatio = Mathf.Tan(Mathf.Deg2Rad * (this.angle / 2.0f));
 
         mf = GetComponent<MeshFilter>();
         mesh = new Mesh();
@@ -40,16 +56,28 @@ public class Wavelet: MonoBehaviour
                               new Vector2(1, 1) };
         mesh.vertices = vertices;
         mesh.uv = uvs;
-        mesh.triangles = new int[] {0, 2, 1, 1, 2, 3};
+        mesh.triangles = new int[] { 0, 2, 1, 1, 2, 3 };
 
         mf.mesh = mesh;
     }
 
     void Update()
     {
-        SetWaveLength(maxLength); // For testing only!
+        phase += speed * Time.deltaTime;
 
-        length = Mathf.Clamp(length + (speed * Time.deltaTime), 0.0f, maxLength);
+        var hit = Physics2D.Raycast(transform.position, transform.up, wavePower);
+        if (hit.collider != null)
+        {
+            SetWaveLength(hit.distance);
+        }
+        else
+        {
+            SetWaveLength(wavePower);
+        }
+        length = Mathf.Clamp(length + (speed * Time.deltaTime) + 0.05f, 0.0f, maxLength);
+
+        tiling = (int)length * waveDensity;
+        leftover = (length - (int)length) * waveDensity;
 
         vertices[2].y = length;
         vertices[2].x = -halfWidth - (length * spreadRatio);
@@ -58,43 +86,23 @@ public class Wavelet: MonoBehaviour
 
         mesh.vertices = vertices;
 
-        if (length != maxLength)
+        if (phase >= 1.0f)
         {
-            TileUVs(length);
+            phase = phase - 1.0f;
         }
-        else
-        {
-            ScrollUVs();
-        }
+
+        uvs[2].y = 1 + leftover - (phase * waveDensity);
+        uvs[3].y = 1 + leftover - (phase * waveDensity);
+        uvs[0].y = 1 - tiling - (phase * waveDensity);
+        uvs[1].y = 1 - tiling - (phase * waveDensity);
+
+        mesh.uv = uvs;
+        
     }
 
     void SetWaveLength(float newMaxLength)
     {
         maxLength = Mathf.Clamp(newMaxLength, 0.0f, wavePower);
-
-        if (maxLength < length)
-        {
-            length = maxLength;
-
-            uvs[2].y = uvs[0].y + (length * waveDensity);
-            uvs[3].y = uvs[1].y + (length * waveDensity);
-        }
     }
 
-    void TileUVs(float length)
-    {
-        uvs[0].y = uvs[2].y - (length * waveDensity);
-        uvs[1].y = uvs[3].y - (length * waveDensity);
-
-        mesh.uv = uvs;
-    }
-
-    void ScrollUVs()
-    {
-        for (int i = 0; i < uvs.Length; i++)
-        {
-            uvs[i].y = uvs[i].y - (speed * Time.deltaTime) * 2f;
-        }
-        mesh.uv = uvs;
-    }
 }
